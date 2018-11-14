4bcc84519bc18c747e10c596d0bb163833487c71
Jal Panchal <jal08panchal@gmail.com>
2018-11-14 05:12:59 -0800

    Copying MapBox keys

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYXZqYWwxMjMiLCJhIjoiY2pvZnR2anpiMDgybTNwbW5xbTViN3N6cCJ9.frqsf0AKtv9xqt7oEq67-w',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
